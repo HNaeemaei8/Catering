9119ac1a9e632d140547e5362544c0a6dd19e99b
ho.naeemaei <ho.naeemaei@HO-NAEEMAEI>
2023-07-03 08:01:56 +0330

    Add project files.

---
+++ Domain/Entities/Food.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Food
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public decimal Rate { get; set; }
+        public byte[] Image { get; set; }
+    }
+}
